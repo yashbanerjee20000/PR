4f8a0127d57a0cc6418be08bfad197f1a53626d0
yashbanerjee20000 <295639840+yashbanerjee20000@users.noreply.github.com>
2026-06-28 19:34:24 +0530

    Add missing test for multiply

--- test_math_utils.py
+++ test_math_utils.py
@@ -1,4 +1,4 @@
-from math_utils import add, subtract, divide
+from math_utils import add, subtract, multiply, divide
 
 
 def test_add():
@@ -7,6 +7,10 @@ def test_add():
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(4, 3) == 12
 
 
 def test_divide():
